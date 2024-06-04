15a0510ca133dd46a05eeae4619c7c508a235938
Emmanuel <emmanuelokello294@gmail.com>
2024-06-04 17:50:12 +0300

    completes code along

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+        
+    def has(self, key):
+        return key in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, key):
+        if key in self.dictionary:
+            del self.dictionary[key]
+            return True
+        return False
+    
+    def size(self):
+        return len(self.dictionary)
+    
 
-    pass
+    def clear(self):
+        self.dictionary = {}
+        return self
+
